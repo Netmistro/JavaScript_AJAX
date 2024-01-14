1acab7516fef5203b6d8b1da62672d5620fe6367
Arnold Bradshaw <arnoldbradshaw@hotmail.com>
2024-01-14 11:31:19 -0400

    Complete AJAX code for GET and POST

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-// Constants
-let getData = document.getElementById("getData");
+// ! Get request in AJAX
 
-// Event Listeners
-getData.addEventListener("click", () => {
-  //   Make a request to the server
-  let xhr = new XMLHttpRequest();
-  // Open the file asynchronusly
-  xhr.open("GET", "ajaxdata.txt", true);
-  xhr.onprogress = function () {
-    console.log("On Progress");
-  };
-  xhr.onload = function () {
-    if (this.status === 200) {
-      console.log(this.responseText);
-    } else if (this.status === 404) {
-      console.log("Not Found");
-    }
-  };
-  //   Send the data
-  xhr.send();
-});
+// // Constants
+// let getData = document.getElementById("getData");
 
-// Functions
+// // Event Listeners
+// getData.addEventListener("click", () => {
+//   //   Make a request to the server
+//   let xhr = new XMLHttpRequest();
+//   // Open the file asynchronusly
+//   xhr.open("GET", "ajaxdata.txt", true);
+//   xhr.onprogress = function () {
+//     console.log("On Progress");
+//   };
+// xhr.onload = function () {
+//   // Echo ready states as 1,2,3,4
+//   console.log("Ready state is + xhr.readyState");
+// };
+//   xhr.onload = function () {
+//     // Status OK
+//     if (this.status === 200) {
+//       console.log(this.responseText);
+//     } else if (this.status === 404) {
+//       // Not found
+//       console.log("Not Found");
+//     }
+//   };
+//   // Send the data
+//   xhr.send();
+// });
+
+// ! POST request in AJAX
+
+// // Constants
+// let getData = document.getElementById("getData");
+
+// // Event Listeners
+// getData.addEventListener("click", () => {
+//   //   Make a request to the server
+//   let xhr = new XMLHttpRequest();
+//   // Open the file asynchronusly
+//   xhr.open("POST", "https://fakestoreapi.com/products", true);
+//   xhr.onprogress = function () {
+//     console.log("On Progress");
+//   };
+//   xhr.onload = function () {
+//     // Echo ready states as 1,2,3,4
+//     console.log("Ready state is + xhr.readyState");
+//   };
+//   xhr.onload = function () {
+//     // Status OK
+//     if (this.status === 200) {
+//       console.log(this.responseText);
+//     } else if (this.status === 404) {
+//       // Not found
+//       console.log("Not Found");
+//     }
+//   };
+//   data = { title: " AJAX Tutorial" };
+//   // Send the data
+//   xhr.send(data);
+// });
